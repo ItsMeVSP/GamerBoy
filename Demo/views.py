8221b821912d7f0a8eba85cbdf8595385b6f567a
ItsMeVSP <saiprasad1.x@gmail.com>
2023-09-30 18:29:51 +0530

    MP5

--- Demo/views.py
+++ Demo/views.py
@@ -3,5 +3,5 @@ from django.shortcuts import render
 
 # Create your views here.
 def home(request):
-    msg="<h1>Naa Ready thaan Varava</h1>"
+    msg="<h1>Annan Naan Erangi Varava</h1>"
     return HttpResponse(msg)
