11909b212d31cbbb2bb0a02a57a05a465223b63b
ItsMeVSP <saiprasad1.x@gmail.com>
2023-10-03 06:26:26 +0530

    Used HTML

--- Demo/views.py
+++ Demo/views.py
@@ -3,5 +3,5 @@ from django.shortcuts import render
 
 # Create your views here.
 def home(request):
-    msg="<h1>Annan Naan Erangi Varava</h1>"
-    return HttpResponse(msg)
+    # msg="<h1>Annan Naan Erangi Varava</h1>"
+    return render(request,'index.html')
